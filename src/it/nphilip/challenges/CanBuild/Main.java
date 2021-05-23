4deeedaa8fad9cd2763f78f852e5393213e7bfbc
Nphilip9 <philip.neumair2020@gmail.com>
2021-05-23 17:37:17 +0200

    added CanBuild project

---
+++ src/it/nphilip/challenges/CanBuild/Main.java
@@ -0,0 +1,19 @@
+package it.nphilip.challenges.CanBuild;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] words = {"Hello", "lohles"};
+        canBuild(words);
+    }
+
+    public static void canBuild(String[] words) {
+        boolean contains = true;
+        for (char ch : words[0].toLowerCase().toCharArray()) {
+            if(!words[1].toLowerCase().contains(String.valueOf(ch))) {
+                contains = false;
+                break;
+            }
+        }
+        System.out.println(words[1] + " can Build the word " + words[1] + ": " + contains);
+    }
+}

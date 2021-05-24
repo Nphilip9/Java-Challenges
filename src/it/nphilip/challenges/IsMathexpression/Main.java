b2d81a08a7b3f9f50eba8fb67e294171047553f0
Nphilip9 <philip.neumair2020@gmail.com>
2021-05-24 23:27:02 +0200

    added IsMathExpression project

---
+++ src/it/nphilip/challenges/IsMathexpression/Main.java
@@ -0,0 +1,70 @@
+package it.nphilip.challenges.IsMathexpression;
+
+public class Main {
+    public static void main(String[] args) {
+        isMathExpression("2*2");
+    }
+
+    public static void isMathExpression(String text) {
+        boolean check = false;
+        String numbers = "1234567890";
+
+        if(text.contains("+")) {
+            for (char textChar : text.toCharArray()) {
+                if(!String.valueOf(textChar).equals("*")) {
+                    if(numbers.contains(String.valueOf(textChar))) {
+                        check = true;
+                    } else {
+                        check = false;
+                        break;
+                    }
+                }
+            }
+        } else if(text.contains("-")) {
+            for (char textChar : text.toCharArray()) {
+                if(!String.valueOf(textChar).equals("*")) {
+                    if(numbers.contains(String.valueOf(textChar))) {
+                        check = true;
+                    } else {
+                        check = false;
+                        break;
+                    }
+                }
+            }
+        } else if(text.contains("*")) {
+            for (char textChar : text.toCharArray()) {
+                if(!String.valueOf(textChar).equals("*")) {
+                    if(numbers.contains(String.valueOf(textChar))) {
+                        check = true;
+                    } else {
+                        check = false;
+                        break;
+                    }
+                }
+            }
+        } else if(text.contains("/")) {
+            for (char textChar : text.toCharArray()) {
+                if(!String.valueOf(textChar).equals("*")) {
+                    if(numbers.contains(String.valueOf(textChar))) {
+                        check = true;
+                    } else {
+                        check = false;
+                        break;
+                    }
+                }
+            }
+        } else if(text.contains("%")) {
+            for (char textChar : text.toCharArray()) {
+                if(!String.valueOf(textChar).equals("*")) {
+                    if(numbers.contains(String.valueOf(textChar))) {
+                        check = true;
+                    } else {
+                        check = false;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(check);
+    }
+}

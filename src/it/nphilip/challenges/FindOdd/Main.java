2d0be902a86fc068375468ad62f8bf2f81b8c83a
Nphilip9 <philip.neumair2020@gmail.com>
2021-06-11 16:53:40 +0200

    added FindOdd project

---
+++ src/it/nphilip/challenges/FindOdd/Main.java
@@ -0,0 +1,16 @@
+package it.nphilip.challenges.FindOdd;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 1, 7, 8};
+        findOdd(arr);
+    }
+
+    public static void findOdd(int[] arr) {
+        for (int element : arr) {
+            if(element % 2 == 1) {
+                System.out.println(element + " is a Odd number");
+            }
+        }
+    }
+}
